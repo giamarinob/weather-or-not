198cf1cefe2d2b6c28552c5441c07180e41f317f
Ben Giamarino <giamarinob@gmail.com>
2016-08-17 15:18:11 +0000

    Went through and deleted extraneous or broken code - added comments - left explanations for broken code I left

--- client/js/app.js
+++ client/js/app.js
@@ -1,1 +1,2 @@
+//app for ng-app directive - called weather app - inject ngResource to handle server calls
 var app = angular.module('weatherApp', ['ngResource'])
--- server/controllers/weather-controller.js
+++ server/controllers/weather-controller.js
@@ -1,9 +1,8 @@
-var key = require('../../key'),
-    request = require('request'),
-    async   = require('async');
+//This is the function the post request called - when called from the client the response successfully sends the temp - hardocded as 20 - 
+//to the callback function on the client which then updates the dom
+//Introducing the ajax call in here was where it all went wrong - need to investigate further
 
-exports.findWeather = function(request, response){
-	var url = 'http://api.wunderground.com/api/' + key.key() + '/geolookup/conditions/q/' + request.body.state + '/' + request.body.city + '.json';
-	console.log(url);
+module.exports.findWeather = function(request, response){
+	// var url = 'http://api.wunderground.com/api/' + key.key() + '/geolookup/conditions/q/' + request.body.state + '/' + request.body.city + '.json';
 	response.send({temp: 20});
 }
